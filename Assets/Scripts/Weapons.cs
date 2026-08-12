c717009deda2eee3d9ac48111a83449c27557f5e
DCGMCoder08 <drizztcalvin@gmail.com>
2026-08-12 09:01:22 +0800

    Start of Weapon Script
    I got started on the weapon script, and I also made the Zombie a prefab.

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class Weapons : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletSpeed = 20f;
+    public float bulletPower;
+    public float shotsPerSecond = 3f;
+    private float fireRate;
+    private float nextFireTime = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        fireRate = 1f / shotsPerSecond;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            Fire();
+        }
+    }
+    void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.linearVelocity = firePoint.forward * bulletSpeed;
     }
 }
