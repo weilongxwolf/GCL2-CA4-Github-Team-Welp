58032bf768cbef2427ecadeb5edbb730d7548ada
weilongxwolf <141908558+weilongxwolf@users.noreply.github.com>
2026-08-12 23:39:39 +0800

    Revert "Merge branch 'main' of https://github.com/weilongxwolf/GCL2-CA4-Github-Team-Welp"
    This reverts commit 3410a1a563ed74c8e8eaf861af1fe4df7a215ae0, reversing
    changes made to 70eb43bfe86c368fab8701ac211191accff461f4.

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -2,61 +2,15 @@ using UnityEngine;
 
 public class Weapons : MonoBehaviour
 {
-    public GameObject bulletPrefab;
-    public Transform firePoint;
-    public float bulletSpeed = 20f;
-    public float bulletPower;
-    public float shotsPerSecond = 3f;
-    private float fireRate;
-    private float nextFireTime = 0f;
-
-    public int maxAmmo = 15;
-    private int currentAmmo;
-    public float reloadTime = 2f;
-    private bool isReloading = false;
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        fireRate = 1f / shotsPerSecond;
-        currentAmmo = maxAmmo;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isReloading)
-        {
-            return;
-        }
-
-        if (currentAmmo <= 0)
-        {
-            StartCoroutine(Reload());
-            return;
-        }
-
-        if (Input.GetButtonDown("Fire1") && Time.time >= nextFireTime)
-        {
-            nextFireTime = Time.time + fireRate;
-            Fire();
-        }
-    }
-    void Fire()
-    {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        rb.linearVelocity = firePoint.forward * bulletSpeed;
-    }
-
-    System.Collections.IEnumerator Reload()
-    {
-        isReloading = true;
-        Debug.Log("Reloading...");
-        yield return new WaitForSeconds(reloadTime);
-        currentAmmo = maxAmmo;
-        isReloading = false;
+        
     }
 }
